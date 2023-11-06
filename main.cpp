f384654c94cec128360fdb10ae83dc9d10013fd1
Z3r0M3m0ry <liqitong@163.com>
2023-11-06 10:22:09 +0800

    [ s2c-build ] add json

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+#include <fstream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
 int main()
 {
+    json j;
     ::std::cout << "Solidity2CPN" << ::std::endl;
 
     return 0;
