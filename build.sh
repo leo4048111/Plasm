2df9c05f101c464ecbbea7e1986da92d02703c42
Z3r0M3m0ry <liqitong@163.com>
2023-11-07 17:35:15 +0800

    [ s2c-compile ] Added build.sh
    [ s2c-compile ] Added build.sh

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+BUILD_DIR=build
+
+if [ -d "$BUILD_DIR" ]; then
+    echo "Removing existing build directory..."
+    rm -rf "$BUILD_DIR"
+fi
+
+echo "Creating build directory..."
+mkdir "$BUILD_DIR"
+
+cd "$BUILD_DIR"
+
+echo "Generating project with CMake..."
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DTESTS=OFF
+
+echo "Building project..."
+make
+
+echo "Build completed."
