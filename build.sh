ad45e406c64efd1456e3f4fd41239160eea842a4
Z3r0M3m0ry <liqitong@163.com>
2023-11-09 11:36:11 +0800

    [ psm-build ] Updated build.sh. supports clang
    [ psm-build ] Updated build.sh. supports clang

--- build.sh
+++ build.sh
@@ -2,6 +2,18 @@
 set -e
 
 BUILD_DIR=build
+BUILD_TYPE=Debug
+CLANG=$(command -v clang)
+CLANGXX=$(command -v clang++)
+CMAKE_C_COMPILER=clang
+CMAKE_CXX_COMPILER=clang++
+
+# Check for clang and clang++
+if [ -z "$CLANG" ] || [ -z "$CLANGXX" ]; then
+  echo "clang or clang++ not found, using default gnu/gcc."
+  CMAKE_C_COMPILER=cc
+  CMAKE_CXX_COMPILER=c++
+fi
 
 if [ -d "$BUILD_DIR" ]; then
     echo "Removing existing build directory..."
@@ -14,7 +26,8 @@ mkdir "$BUILD_DIR"
 cd "$BUILD_DIR"
 
 echo "Generating project with CMake..."
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DTESTS=OFF
+cmake .. -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DTESTS=OFF -DUSE_Z3=OFF \
+-DCMAKE_C_COMPILER="$CMAKE_C_COMPILER" -DCMAKE_CXX_COMPILER="$CMAKE_CXX_COMPILER"
 
 echo "Building project..."
 make -j8
