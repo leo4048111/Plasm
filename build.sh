91ea961e31b43871d53dd0bb39b9a5bd0cd29155
Z3r0M3m0ry <liqitong@163.com>
2023-11-10 16:27:19 +0800

    [ psm-build ] clang will generate illegal instruction errors, disabled for now

--- build.sh
+++ build.sh
@@ -5,15 +5,15 @@ BUILD_DIR=build
 BUILD_TYPE=Debug
 CLANG=$(command -v clang)
 CLANGXX=$(command -v clang++)
-CMAKE_C_COMPILER=clang
-CMAKE_CXX_COMPILER=clang++
+CMAKE_C_COMPILER=cc
+CMAKE_CXX_COMPILER=c++
 
 # Check for clang and clang++
-if [ -z "$CLANG" ] || [ -z "$CLANGXX" ]; then
-  echo "clang or clang++ not found, using default gnu/gcc."
-  CMAKE_C_COMPILER=cc
-  CMAKE_CXX_COMPILER=c++
-fi
+# if [ "$CLANG" ] || [ "$CLANGXX" ]; then
+#   echo "clang or clang++ found, using clang/clang++."
+#   CMAKE_C_COMPILER=clang
+#   CMAKE_CXX_COMPILER=clang++
+# fi
 
 if [ -d "$BUILD_DIR" ]; then
     echo "Removing existing build directory..."
@@ -30,7 +30,7 @@ cmake .. -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DTESTS=OFF -DUSE_Z3=OFF \
 -DCMAKE_C_COMPILER="$CMAKE_C_COMPILER" -DCMAKE_CXX_COMPILER="$CMAKE_CXX_COMPILER"
 
 echo "Building project..."
-make -j8
+make -j32
 
 echo "Build completed."
 
