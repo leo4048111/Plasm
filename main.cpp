315ef5ccdafc90ea7c042694e9989d781f37ddd6
Yowen397 <137564817@qq.com>
2023-11-23 23:40:40 +0800

    test commit for modeling

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main(int argc, char** argv)
 {
+    std::cout << "test output for modeling branch" << std::endl;
     auto& cli = PSM_NM::CLI::GetInstance();
     cli.Run(argc, argv);
     return 0;
