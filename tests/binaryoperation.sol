4775a44e98de48b37c91829cf3d2430d8ebd3ab4
Z3r0M3m0ry <liqitong@163.com>
2024-04-03 15:53:26 +0800

    [ psm-tests ] updated binaryoperation

--- tests/binaryoperation.sol
+++ tests/binaryoperation.sol
@@ -2,17 +2,12 @@
 pragma solidity >0.4.23;
 
 contract Baseline {
-    // function definition
-    function add(uint256 x, uint256 y) public pure returns(uint256 ret)
-    {
+    function add(uint256 x, uint256 y) public pure returns (uint256 ret) {
         ret = x + y;
         return ret;
     }
 
-    function sub(uint256 x, uint256 y) public pure returns(uint256 ret)
-    {
-        ret = x - y;
-        return ret;
+    function sub(uint256 x, uint256 y) public pure returns (uint256 result) {
+        result = x - y;
     }
 }
-
