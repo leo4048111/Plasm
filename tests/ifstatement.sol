53bff972b0b833940051f775b04ede6694d00c47
Z3r0M3m0ry <liqitong@163.com>
2024-04-08 15:07:48 +0800

    [ psm-tests ] Updated ifStatement

--- tests/ifstatement.sol
+++ tests/ifstatement.sol
@@ -3,10 +3,10 @@ pragma solidity >0.4.23;
 
 contract Baseline {
     function greater(uint256 x, uint256 y) public pure returns (bool result) {
-        if(x > y) {
-            result = true;
-        } else {
-            result = false;
+        if(true) {
+        }
+        else {
+
         }
     }
 }
